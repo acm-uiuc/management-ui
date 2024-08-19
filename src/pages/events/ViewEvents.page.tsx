61234b4355072fdee5a9ce38b14d25b29befe8ea
Pete Stenger <pete@stenger.io>
2024-08-19 16:36:41 -0500

    switch to table, add delete functionality

--- src/pages/events/ViewEvents.page.tsx
+++ src/pages/events/ViewEvents.page.tsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { Title, Box, Card, Text, Grid, SimpleGrid, Button, Flex } from '@mantine/core';
+import { Title, Box, Card, Text, Grid, SimpleGrid, Button, Flex, Table } from '@mantine/core';
 import { z } from 'zod';
 import dayjs from 'dayjs';
 import { useApi } from '@/util/api';
 import { getRunEnvironmentConfig } from '@/config';
 import { AuthGuard } from '@/components/AuthGuard';
 import FullScreenLoader from '@/components/AuthContext/LoadingScreen';
+import { notifications } from '@mantine/notifications';
 
 const repeatOptions = ['weekly', 'biweekly'] as const;
 
@@ -35,17 +36,38 @@ const getEventsSchema = z.array(getEventSchema);
 export type EventsGetResponse = z.infer<typeof getEventsSchema>;
 
 export const ViewEventsPage: React.FC = () => {
-  const { friendlyName } = getRunEnvironmentConfig().ServiceConfiguration.events;
   const [eventList, setEventList] = useState<EventsGetResponse>([]);
   const api = useApi('events');
 
   useEffect(() => {
     const getEvents = async () => {
       const response = await api.get('/api/v1/events');
+      const events = response.data;
+      events.sort((a: EventGetResponse, b: EventGetResponse) => {
+        return a.start.localeCompare(b.start);
+      });
       setEventList(response.data);
     };
     getEvents();
   }, []);
+
+  const deleteEvent = async (eventId: string) => {
+    try {
+      const response = await api.delete(`/api/v1/events/${eventId}`);
+      setEventList((prevEvents) => prevEvents.filter((event) => event.id !== eventId));
+      notifications.show({
+        title: 'Event deleted',
+        message: `The event was successfully deleted.`,
+      });
+    } catch (error) {
+      console.error(error);
+      notifications.show({
+        title: 'Error deleting event',
+        message: `${error}`,
+        color: 'red',
+      });
+    }
+  };
 
   if (eventList.length === 0) {
     return <FullScreenLoader />;
@@ -54,27 +76,54 @@ export const ViewEventsPage: React.FC = () => {
   return (
     <AuthGuard resourceDef={{ service: 'events', validRoles: ['manage:events'] }}>
       <Title order={2}>All Events</Title>
-      <SimpleGrid cols={3}>
-        {eventList.map((event) => (
-            <Card key={event.id} shadow="xs" padding="md" radius="md" style={{ marginBottom: 20 }}>
-              <Flex direction="row" justify="space-between">
-                <Title order={4}>{event.title}</Title>
+      <Table>
+        <Table.Thead>
+          <Table.Tr>
+            <Table.Th>Title</Table.Th>
+            <Table.Th>Start</Table.Th>
+            <Table.Th>End</Table.Th>
+            <Table.Th>Location</Table.Th>
+            <Table.Th>Description</Table.Th>
+            <Table.Th>Host</Table.Th>
+            <Table.Th>Featured</Table.Th>
+            <Table.Th>Repeats</Table.Th>
+            <Table.Th />
+            <Table.Th />
+          </Table.Tr>
+        </Table.Thead>
+        <Table.Tbody>
+          {eventList.map((event) => (
+            <Table.Tr key={event.id}>
+              <Table.Td>{event.title}</Table.Td>
+              <Table.Td>{dayjs(event.start).format('MMM D YYYY hh:mm')}</Table.Td>
+              <Table.Td>{event.end ? dayjs(event.end).format('MMM D YYYY hh:mm') : 'N/A'}</Table.Td>
+              <Table.Td>{event.location}</Table.Td>
+              <Table.Td>{event.description}</Table.Td>
+              <Table.Td>{event.host}</Table.Td>
+              <Table.Td>{event.featured ? 'Yes' : 'No'}</Table.Td>
+              <Table.Td>{event.repeats}</Table.Td>
+              <Table.Td>
                 <Button
                   component="a"
                   href={`/events/edit/${event.id}`}
                 >
                   Edit
                 </Button>
-              </Flex>
-              <Text>{dayjs(event.start).format('MMMM D, YYYY')}</Text>
-              <Text>{event.location}</Text>
-              <Text>{event.description}</Text>
-              <Text>{event.host}</Text>
-              {event.featured && <Text>Featured</Text>}
-              {event.repeats && <Text>Repeats "{event.repeats}"</Text>}
-            </Card>
-        ))}
-      </SimpleGrid>
+              </Table.Td>
+              <Table.Td>
+                <Button
+                  color="red"
+                  onClick={() => {
+                    deleteEvent(event.id);
+                  }}
+                >
+                  Delete
+                </Button>
+              </Table.Td>
+            </Table.Tr>
+          ))}
+        </Table.Tbody>
+      </Table>
     </AuthGuard>
   );
 };
